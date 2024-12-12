b484dc3343be0668c2efa30156821cbfeaf2af95
ImNotGabo <gabriel.duarte18@outlook.es>
2024-12-11 22:12:53 -0400

    Event loop in practice

--- event-loop.js
+++ event-loop.js
@@ -2,6 +2,7 @@ const fs = require('fs');
 const crypto = require('crypto');
 
 const start = Date.now();
+process.env.UV_THREADPOOL_SIZE = 1;
 
 // No event loop
 setTimeout(() => console.log('Timer 1 finished'), 0);
@@ -18,9 +19,6 @@ fs.readFile('test-file.txt', () => {
 
 	process.nextTick(() => console.log('Process.nextTick'));
 
-	crypto.pbkdf2('password', 'salt', 100000, 1024, 'sha512', () =>
-		console.log(Date.now() - start, 'Password encrypted')
-	);
 	crypto.pbkdf2('password', 'salt', 100000, 1024, 'sha512', () =>
 		console.log(Date.now() - start, 'Password encrypted')
 	);
