2d015419a50d533610b7736d55036e4355c631f9
ImNotGabo <gabriel.duarte18@outlook.es>
2024-12-15 20:33:04 -0400

    Added sum of 2 numbers

--- modules.js
+++ modules.js
@@ -1,4 +1,7 @@
-console.log(arguments);
-console.log(require('module').wrapper);
+// console.log(arguments);
+// console.log(require('module').wrapper);
 
 const C = require('./test-module-1');
+
+const calculator1 = new C();
+console.log(calculator1.add(2, 5));
