70935f578d28ef16b372dea4f3aa8f3677930fe5
ImNotGabo <gabriel.duarte18@outlook.es>
2024-12-15 20:54:54 -0400

    Normal import along destructuring variables

--- modules.js
+++ modules.js
@@ -8,3 +8,9 @@ const calculator1 = new C();
 console.log(calculator1.add(2, 5));
 
 // exports
+// const calc2 = require('./test-module-2');
+// console.log(calc2.multiply(5, 5));
+
+// Destructured
+const { add, multiply, divide } = require('./test-module-2');
+console.log(multiply(5, 5));
